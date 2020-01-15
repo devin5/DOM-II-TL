e58e0c2f25ea7af16b999aa7ce6e54604a29135b
Devin Dias <devindias.tech@gmail.com>
2020-01-15 08:09:01 -0600

    im a subject line
    im body

--- js/index.js
+++ js/index.js
@@ -1,44 +1,54 @@
 // Your code goes here
 
-window.addEventListener("load", e => alert("welcome"))
+window.addEventListener("load", e => alert("welcome"));
 
-window.addEventListener("resize", e => alert("your connected to server"))
+window.addEventListener("resize", e => alert("your connected to server"));
 
-window.addEventListener("copy", e => alert("dont steal form me"))
+window.addEventListener("copy", e => alert("dont steal form me"));
 
-window.addEventListener('keydown', e => keyDown.style.backgroundColor = "red")
+window.addEventListener(
+  "keydown",
+  e => (keyDown.style.backgroundColor = "red")
+);
 
-let headerC = document.querySelector("header")
-let anchors = document.querySelectorAll('nav a');
-let mainheader = document.querySelector('header div h1');
-let img1 = document.querySelector('.img-content');
-let dragDrop = document.querySelector('.img-fluid');
-let dblClick = document.querySelector('.btn')
+let headerC = document.querySelector("header");
+let anchors = document.querySelectorAll("nav a");
+let mainheader = document.querySelector("header div h1");
+let img1 = document.querySelector(".img-content");
+let dragDrop = document.querySelector(".img-fluid");
+let dblClick = document.querySelector(".btn");
 let selection = document.querySelector(".text-content");
 
+for (var i = 0; i < mouseOver.length; i++) {
+  mouseOver[i].style.backgroundColor = "red";
+}
 
-for( var i=0; i < mouseOver.length; i++){
-    mouseOver[i].style.backgroundColor = "red"
+img1.addEventListener("wheel", e => (img1.style.transform = "skewY(30deg)"));
 
-  }
+dragDrop.addEventListener(
+  "drag",
+  e => (dragDrop.style.border = "10px  solid black")
+);
 
-  img1.addEventListener('wheel', e => img1.style.transform = "skewY(30deg)")
+dragDrop.addEventListener(
+  "mouseleave",
+  e => (dragDrop.style.border = "15px dashed blue")
+);
 
-  dragDrop.addEventListener('drag', e => dragDrop.style.border = "10px  solid black")
+dblClick.addEventListener(
+  "dblclick",
+  e => (dblClick.style.backgroundColor = "red")
+);
 
-  dragDrop.addEventListener('mouseleave', e => dragDrop.style.border ="15px dashed blue")
+selection.addEventListener("select", e => alert("you"));
 
-  dblClick.addEventListener("dblclick", e => dblClick.style.backgroundColor = "red")
+headerC.addEventListener("mouseover", e => {
+  headerC.style.backgroundColor = "blue";
+  event.stopPropagation();
+});
 
-  selection.addEventListener("select", e => alert("you"))
-
-  headerC.addEventListener('mouseover', e => {
-    headerC.style.backgroundColor = "blue"
-    event.stopPropagation();
-  })
-
-  mouseOver.forEach( (a) => {
-    a.addEventListener('click', (event) => {
-          event.preventDefault()
-    })
-})
+mouseOver.forEach(a => {
+  a.addEventListener("click", event => {
+    event.preventDefault();
+  });
+});
